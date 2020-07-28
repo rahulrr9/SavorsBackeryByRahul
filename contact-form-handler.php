62435c136f6c3020cbc6b20625d22cf2dd510934
Rahul Roshan <64370534+rahulrr9@users.noreply.github.com>
2020-07-28 21:20:29 +0500

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,18 +1,18 @@
 <?php
-     $firstname = $_POST{'firstname'};
-     $visitor_email = $_POST{'email'};
+    $firstname = $_POST{'firstname'};
+    $lastname = $_POST{'lastname'};
+    $visitor_email = $_POST{'email'};
      $mobile = $_POST{'phone-no'};
-     $country = $_POST{'country'};
      $message = $_POST{'message'};
 
-     $email_from = 'https://noman258.github.io/Findiway';
+     $email_from = 'https://rahulrr9.github.io/SavorsBackeryByRahul';
      
      $email_subject = "New Form Submission";
 
-     $email_body = "User Name : $firstname . \n".
+     $email_body = "User First Name : $firstname . \n".
+                     "User First Name : $lastname . \n".          
                      "User Email : $visitor_email. \n".
                      "User Mobile Number : $mobile. \n".
-                     "User Country : $country. \n".
                      "User Message : $message. \n";
 
     $to = "tecnocity8@gmail.com";
